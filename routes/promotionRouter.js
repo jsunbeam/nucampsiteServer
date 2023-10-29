5c8f72a67d6f58747ee7ab542903337edabd3076
Jessica Shao <jshaodev@gmail.com>
2023-10-29 14:11:04 -0500

    Renamed promotions.js

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Promotion = require("../models/promotions");
+const Promotion = require("../models/promotion");
 const promotionRouter = express.Router();
 
 promotionRouter
