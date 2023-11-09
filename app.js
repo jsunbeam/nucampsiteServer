812143e9937ece61a79594722ee5a27bc6f58e55
Jessica Shao <jshaodev@gmail.com>
2023-11-08 19:28:18 -0600

    File Upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const config = require("./config");
+const uploadRouter = require("./routes/uploadRouter");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -62,6 +63,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
